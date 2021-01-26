78b77ce701b71d6728f42ec24d321e0c8b33f835
kamalwc <carmichaelkw@gmail.com>
2021-01-25 22:53:34 -0500

    pulled master

--- src/main/ui/ui/src/Home.js
+++ src/main/ui/ui/src/Home.js
@@ -33,18 +33,7 @@ function Home(){
         setCurrMonth(moment(currMonth).subtract(1,'M').format('MMMM YYYY'))
     }
 
-    const renderDays = () =>{
-        let days = [];
 
-        for(let i = 0; i < 7; i++){
-            days.push(<div>{moment().day("Sunday").add(i,'d').format('dddd')}</div>)
-        }
-        return(
-            <div id={"days"}>
-                {days}
-            </div>
-        )
-    }
 
     const renderCells = () =>{
         // start of the month
@@ -87,12 +76,13 @@ function Home(){
         setCurrMonth(moment(currMonth).subtract(1, "months").format('MMMM YYYY'))
     }
 
-    const wheel = (e) =>{
-        if(e.deltaY > 0){
+    const wheel = (e) => {
+        if (e.deltaY > 0) {
             sub();
-        }else if(e.deltaY < 0){
+        } else if (e.deltaY < 0) {
             add();
         }
+    }
 
     const renderDays = () =>{
         let days = [];
@@ -105,39 +95,6 @@ function Home(){
                 {days}
             </div>
         )
-    }
-
-    const renderCells = () =>{
-      // start of the month
-    // start of the week of the month
-    // last day of the month
-    // last day of the week of the last day of the month
-    const monthStart = moment(currMonth).startOf('month');
-    const monthEnd = moment(currMonth).endOf('month');
-    const calendarStart = moment(monthStart).startOf('week');
-    const calendarEnd = moment(monthEnd).endOf('week');
-
-        // while day doestn equal last day render cells
-        let day = calendarStart;
-        let rows = []
-        while(day < calendarEnd){
-            let tempRow = [];
-            for (let i = 0; i < 7; i++) {
-                tempRow.push(<div className={moment(currMonth).isSame(day,'month')? "cells" : "cells-disabled"}>
-                                    {day.format('D')}
-                            </div>)
-                day.add(1,'days');
-            }
-            {rows.push(<div style={{display: "flex", justifyContent:"left"}}>{tempRow}</div>)}
-            tempRow = [];
-        }
-
-        return(
-            <div>
-                {rows}
-            </div>
-        )
-
     }
 
     return(
